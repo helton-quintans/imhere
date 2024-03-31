4d372df80f3d31ecde2a8cee70130aa0e81fecbc
helton-quintans <helton.quit@gmail.com>
2024-03-31 20:19:01 -0300

    refactor: create home

--- App.tsx
+++ App.tsx
@@ -1,30 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    padding: 24,
-    
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6b6b6b',
-  }
-});
