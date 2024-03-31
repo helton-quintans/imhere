b20c56a700e5ff8ca9df9169b7eac9a03cc76202
helton-quintans <helton.quit@gmail.com>
2024-03-31 20:09:20 -0300

    styles: conteiner
    Signed-off-by: helton-quintans <helton.quit@gmail.com>

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#333',
+    padding: 24,
+    
   },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6b6b6b',
+  }
 });
