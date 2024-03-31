7ab918101d445ae174c7a86fba39a0735f73fa0a
helton-quintans <helton.quit@gmail.com>
2024-03-31 20:32:40 -0300

    feat: add input

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
       <StatusBar style="auto" />
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Domingo, 31 de março de 2024</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder='Nome do participante'    
+        placeholderTextColor="#6B6B6B"  
+      />
     </View>
   );
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -8,12 +8,20 @@ export const styles = StyleSheet.create({
       
     },
     eventName: {
-      color: '#FDFCFE',
+      color: '#fff',
       fontSize: 16,
       fontWeight: 'bold',
       marginTop: 48
     },
     eventDate: {
       color: '#6b6b6b',
+    },
+    input: {
+      height: 56,
+      backgroundColor: '#1f1e25',
+      borderRadius: 8,
+      color: '#fff',
+      padding: 16,
+      fontSize: 16
     }
 });
